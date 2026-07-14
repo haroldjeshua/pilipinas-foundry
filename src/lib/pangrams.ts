b18f1b0690466bd0830adb11c9fde465afeb7353
Harold Varde <34917956+haroldjeshua@users.noreply.github.com>
2026-07-14 23:51:12 +0800

    chore: remove pangrams.ts, consolidated to font-pangrams.ts

--- src/lib/pangrams.ts
+++
@@ -1,6 +0,0 @@
-const TAGALOG_PANGRAM =
-  "Ang bagong sudyante ay mabilis na natuto ng mga kumplikadong salita sa Filipino.";
-const ENGLISH_PANGRAM =
-  "The quick brown fox jumps over the lazy dog near the bank of the river.";
-
-export { TAGALOG_PANGRAM, ENGLISH_PANGRAM };
